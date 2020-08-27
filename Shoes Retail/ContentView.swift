50efe0cd7e83e3b7d9a66f502452afef65f54ffe
Caleb Wells <caleb@yeshog.com>
2020-08-26 20:15:49 -0600

    Initial Commit

---
+++ Shoes Retail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shoes Retail
+//
+//  Created by Caleb Wells on 8/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shoes Retail/Shoes_RetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoes_RetailApp.swift
+//  Shoes Retail
+//
+//  Created by Caleb Wells on 8/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Shoes_RetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
